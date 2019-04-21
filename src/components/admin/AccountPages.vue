70f5c35c1f382100177c376e897cc7887fdca953
Igor Alves <igorsk89@gmail.com>
2019-04-21 01:35:03 -0300

    Removed courses on accounts pages

--- src/components/admin/AccountPages.vue
+++ src/components/admin/AccountPages.vue
@@ -5,9 +5,6 @@
       <div class="admin-pages-tabs">
         <b-card no-body>
           <b-tabs card pills>
-            <b-tab title="Minicursos">
-              <h1 class="text-center">Minicursos</h1>
-            </b-tab>
             <b-tab title="Dados Pessoais">
               <PersonalData/>
             </b-tab>
