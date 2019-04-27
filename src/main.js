39eb7352fb9b05592e8f671e412d946993cd6bf6
Igor Alves <igorsk89@gmail.com>
2019-04-27 17:10:12 -0300

    fontawesome via npm

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App'
 import store from './store'
 import router from './router'
 
+import '@fortawesome/fontawesome-free'
+
 // plugins
 import './plugins/bootstrap'
 import './plugins/icons'
